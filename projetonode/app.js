c48f9654042b9778cd28cfd361206bd80989ad44
Hercules Lima <hl3058317@gmail.com>
2019-10-18 15:55:06 -0300

    Template Helpers

--- projetonode/app.js
+++ projetonode/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mustache = require("mustache-express");
 const router = require('./routes/index');
+const helpers = require('./helpers');
 
 // Configurações
 const app = express();
+
+app.use((req, res, next)=>{
+    res.locals.h = helpers;
+    res.locals.teste = '123';
+    next();
+});
+
 app.use('/', router);
 
 app.use(express.json());
--- projetonode/routes/index.js
+++ projetonode/routes/index.js
@@ -3,16 +3,7 @@ const express = require('express');
 const router = express.Router();
 router.get('/', (req, res)=>{
     let obj = {
-        nome:req.query.nome,
-        idade:req.query.idade,
-        mostrar:true,
-        ingredientes:[
-            {nome:'Arroz', qt:'20g'},
-            {nome:'Feijão', qt:'30g'},
-            {nome:'Macarrão', qt:'50g'}
-        ],
-        interesses:['node','js','css'],
-        teste:'<strong>Testando Negrito</strong>'
+        //pageTitle:'Página Padrão'
     };
     res.render('home', obj);
 });
